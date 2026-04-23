c1594b80e4942d4a24625acbb806d591e2f418c7
Javier <javier.carranza.all@gmail.com>
2026-04-22 18:11:33 -0600

    Version 9

--- CarranzaHernandez/src/environments/environment.ts
+++ CarranzaHernandez/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://familia-production-b116.up.railway.app/integrantes',
+  apiBaseUrl: 'https://familia-production-b116.up.railway.app',
 };
